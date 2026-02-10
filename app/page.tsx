09e99a301069db15072c9925d4da4745f1566dc5
Rogelio <gomez.rogelio@nokia.com>
2026-02-09 20:24:10 -0600

    adding shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
+'use client'
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [clicked, setClicked]= useState(false);
+
+  const handleClick = ()=>{
+    setClicked(!clicked)
+  }
+
+
+
   return (
-      <section>
-        <h1>Hello world</h1>
+      <section className="flex flex-col items-center justify-center h-screen">
+        <Button
+          onClick={handleClick}
+        
+        >Handle Click</Button>
+       {clicked? <h1>Hello world</h1>:<p></p>} 
       </section>
     
   );
